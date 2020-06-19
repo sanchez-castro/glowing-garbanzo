2695c6c34b14b2e321ec85b9c212940ea7cc5223
Jose Smiller <jose.smiller@axented.com>
2020-06-19 17:46:12 -0500

    Add sidebar toggling logic

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,35 +1,81 @@
-import React, { Component } from 'react'
-import Tree from './tree'
-import styles from './sidebar.module.scss'
+import React, { useState } from "react";
+import Tree from "./tree";
+import styles from "./sidebar.module.scss";
 
-const Sidebar = () => {
-
-    const refs = Array(3).fill(0).map(() => React.useRef<Tree>() as React.MutableRefObject<Tree>);
-
-    const handleToggle = () => {
-        refs.forEach( ref => {
-            const node = ref.current
-            if(node) {
-                node.toggleMenu()
-            }
-        })
-    }
-
-    return (
-        <div className={styles.bar}>
-            <p className={styles.title}>Data-Collective</p>
-            <p className={styles.bio}>I spend my time trying to change the way we understand social phenomenon through data science to resolve problems for social good.</p>
-            
-            <div className={styles.toggleTitle} onClick={handleToggle}>
-                Learning topics
-                <img src={'icon/arrow-ios-up.svg'} alt="collapse arrow"/>
-            </div>
-
-            <Tree ref={refs[0]} parent={"DATA SCIENCE"} nodes={['DATA MINING']}></Tree>
-            <Tree ref={refs[1]} parent={"MACHINE LEARNING"} nodes={['SUPERVISED LEARNING', 'CLUSTERING', 'K-MODES']}></Tree>
-            <Tree ref={refs[2]} parent={"DEEP LEARNING"} nodes={[]}></Tree>
-        </div>
-    )
+interface LayoutProps {
+  hideHandler: (event: React.MouseEvent<HTMLImageElement>) => void;
+  hidden: boolean;
 }
 
-export default Sidebar
+const Sidebar = (props: LayoutProps) => {
+  const refs = Array(3)
+    .fill(0)
+    .map(() => React.useRef<Tree>() as React.MutableRefObject<Tree>);
+  const [tree, setTree] = useState({ extended: true });
+
+  const handleToggle = () => {
+    refs.forEach((ref) => {
+      const node = ref.current;
+      if (node) {
+        node.toggleMenu();
+      }
+    });
+
+    setTree({
+      extended: !tree.extended,
+    });
+  };
+
+  return (
+    <div className={props.hidden ? styles.hiddenBar : styles.bar}>
+      <div className={styles.titleContainer}>
+        <p className={styles.title}>Data-Collective</p>
+        {!props.hidden ? <div className={styles.titleBackground}></div> : ""}
+      </div>
+      <p className={styles.bio}>
+        I spend my time trying to change the way we understand social phenomenon
+        through data science to resolve problems for social good.
+      </p>
+
+      <img
+        className={[props.hidden ? styles.rotated : "", styles.hideButton].join(
+          " "
+        )}
+        onClick={props.hideHandler}
+        src={"icon/hide-sidebar.svg"}
+        alt=""
+      />
+
+      <div className={styles.toggleTitle} onClick={handleToggle}>
+        Learning topics
+        <img
+          className={tree.extended ? styles.rotated : ""}
+          src={"icon/arrow-ios-up.svg"}
+          alt="collapse arrow"
+        />
+      </div>
+
+      <div className={styles.treeContainer}>
+        <Tree
+          ref={refs[0]}
+          parent={"DATA SCIENCE"}
+          nodes={["DATA MINING"]}
+        ></Tree>
+      </div>
+
+      <div className={styles.treeContainer}>
+        <Tree
+          ref={refs[1]}
+          parent={"MACHINE LEARNING"}
+          nodes={["SUPERVISED LEARNING", "CLUSTERING", "K-MODES"]}
+        ></Tree>
+      </div>
+
+      <div className={styles.treeContainer}>
+        <Tree ref={refs[2]} parent={"DEEP LEARNING"} nodes={[]}></Tree>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
