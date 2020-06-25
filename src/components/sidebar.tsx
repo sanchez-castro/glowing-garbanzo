88737f487435f751d25d46d41df988fa55a80373
Jose Smiller <jose.smiller@axented.com>
2020-06-24 19:56:27 -0500

    Move collapsable menu component to sidebar file

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,81 +1,71 @@
 import React, { useState } from "react";
-import Tree from "./tree";
+import { Collapse } from 'react-bootstrap'
 import styles from "./sidebar.module.scss";
 
 interface LayoutProps {
-  hideHandler: (event: React.MouseEvent<HTMLImageElement>) => void;
   hidden: boolean;
 }
 
 const Sidebar = (props: LayoutProps) => {
-  const refs = Array(3)
-    .fill(0)
-    .map(() => React.useRef<Tree>() as React.MutableRefObject<Tree>);
   const [tree, setTree] = useState({ extended: true });
 
   const handleToggle = () => {
-    refs.forEach((ref) => {
-      const node = ref.current;
-      if (node) {
-        node.toggleMenu();
-      }
-    });
-
     setTree({
       extended: !tree.extended,
     });
   };
 
   return (
-    <div className={props.hidden ? styles.hiddenBar : styles.bar}>
+    <div className={props.hidden ? styles.hiddenBar : ''}>
       <div className={styles.titleContainer}>
-        <p className={styles.title}>Data-Collective</p>
-        {!props.hidden ? <div className={styles.titleBackground}></div> : ""}
-      </div>
-      <p className={styles.bio}>
-        I spend my time trying to change the way we understand social phenomenon
-        through data science to resolve problems for social good.
-      </p>
-
-      <img
-        className={[props.hidden ? styles.rotated : "", styles.hideButton].join(
-          " "
-        )}
-        onClick={props.hideHandler}
-        src={"icon/hide-sidebar.svg"}
-        alt=""
-      />
-
-      <div className={styles.toggleTitle} onClick={handleToggle}>
-        Learning topics
-        <img
-          className={tree.extended ? styles.rotated : ""}
-          src={"icon/arrow-ios-up.svg"}
-          alt="collapse arrow"
-        />
+        <p className={styles.title}>Data <br/> Product <br/> Design</p>
+        <p className={styles.bio}>
+          I spend my time trying to change the way we understand social phenomenon
+          through data science to resolve problems for social good.
+        </p>
       </div>
 
-      <div className={styles.treeContainer}>
-        <Tree
-          ref={refs[0]}
-          parent={"DATA SCIENCE"}
-          nodes={["DATA MINING"]}
-        ></Tree>
-      </div>
+      <div className={styles.learningTopics}>
+        <div className={styles.topics} >Learning topics</div>
 
-      <div className={styles.treeContainer}>
-        <Tree
-          ref={refs[1]}
-          parent={"MACHINE LEARNING"}
-          nodes={["SUPERVISED LEARNING", "CLUSTERING", "K-MODES"]}
-        ></Tree>
-      </div>
-
-      <div className={styles.treeContainer}>
-        <Tree ref={refs[2]} parent={"DEEP LEARNING"} nodes={[]}></Tree>
+        <CollapsableMenu parent={'Data Science'} nodes={['Data Mining']}></CollapsableMenu>
+        <CollapsableMenu parent={'Machine Learning'} nodes={['Supervised Learning', 'Clustering', 'K-Modes']}></CollapsableMenu>
+        <CollapsableMenu parent={'Deep Learning'} nodes={[]}></CollapsableMenu>
+        <CollapsableMenu parent={'Business Intelligence'} nodes={[]}></CollapsableMenu>
+        <CollapsableMenu parent={'Technology'} nodes={[]}></CollapsableMenu>
       </div>
     </div>
   );
 };
+
+interface MenuProps {
+  parent: string;
+  nodes: Array<string>;
+}
+
+const CollapsableMenu = (props: MenuProps) => {
+  const [open, setOpen] = useState(false);
+  
+  return (
+    <div className={styles.collapsable}>
+      <div
+        className={styles.header}
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}>
+          {props.parent}
+      </div>
+      <Collapse in={open}>
+        <div>
+          {props.nodes.map((node) => (
+            <div key={node} className={styles.node}>
+              {node}
+            </div>
+          ))}
+        </div>
+    </Collapse>
+    </div>
+  );
+}
 
 export default Sidebar;
