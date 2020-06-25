5a3fe919de5677f771895a137fc7ca2fb480c294
Jose Smiller <jose.smiller@axented.com>
2020-06-24 20:00:51 -0500

    Add post preview component to index (WIP)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
+import PostPreview from '../components/post-preview'
 import SEO from "../components/seo";
+import styles from './index.module.scss'
+import "../global.scss";
+import { Card, Row, Col, Container } from "react-bootstrap";
 
 interface Props {
   data: {
@@ -18,10 +22,36 @@ const BlogIndex = ({ data }: Props) => {
   const siteTitle = data.site.siteMetadata.title;
   const posts = data.allMarkdownRemark.edges;
 
+  const mockPost = {
+    title: "Getting and Cleaning Data",
+    date: "June 10, 2020",
+    contentPreview: "In this post you will get an introduction to the main tools and ideas in the data scientist's toolbox. The course gives an overview of the data, questions, and tools that data analysts and data scientists work with…",
+    postTypes: ['Article', 'Video'],
+    tags: ['Data Science', 'AI', 'Business'],
+    imagePath: "https://picsum.photos/500/700"
+  }
+
   return (
     <Layout location={window.location} title={siteTitle}>
+      <Container fluid>
+        <LearningPaths></LearningPaths>
+        <Row className="justify-content-md-center">
+          <Col lg={10} xl={10}>
+            <Row className={styles.explore}>
+              <Col className={styles.title}>
+                Explore
+              </Col>
+            </Row>
+            <PostPreview post={mockPost} extended={true}></PostPreview>
+            <PostPreview post={mockPost}></PostPreview>
+            <PostPreview post={mockPost}></PostPreview>
+            <PostPreview post={mockPost}></PostPreview>
+            <PostPreview post={mockPost}></PostPreview>
+          </Col>
+        </Row>
+      </Container>
       <SEO title="All posts" />
-      {posts.map(({ node }: any) => {
+      {/* {posts.map(({ node }: any) => {
         const title = node.frontmatter.title || node.fields.slug;
         return (
           <div key={node.fields.slug}>
@@ -42,10 +72,51 @@ const BlogIndex = ({ data }: Props) => {
             />
           </div>
         );
-      })}
+      })} */}
     </Layout>
   );
 };
+
+const LearningPaths = () => {
+  return (
+    <Row className={["justify-content-md-center", styles.learningPaths].join(" ")}>
+      <Col lg={12} xl={12} className={styles.title}>
+        Learning paths
+      </Col>
+      <Col lg={12} xl={12} className={styles.cards}>
+        <Card className={styles.card}>
+          <Card.Body>
+          <Card.Title className={styles.cardImage}><img src={"icon/data-scientist.svg"}/></Card.Title>
+          <Card.Subtitle className={styles.cardTitle}>Data Scientist</Card.Subtitle>
+          <Card.Text className={styles.cardText}>
+            With this path you will learn how to become a great data scientist.
+          </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className={styles.card}>
+          <Card.Body>
+          <Card.Title className={styles.cardImage}><img src={"icon/data-analyst.svg"}/></Card.Title>
+          <Card.Subtitle className={styles.cardTitle}>Data Analyst</Card.Subtitle>
+          <Card.Text className={styles.cardText}>
+            With this path you will learn how to become a great data analyst.
+          </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className={styles.card}>
+          <Card.Body>
+          <Card.Title className={styles.cardImage}><img src={"icon/bi-analyst.svg"}/></Card.Title>
+          <Card.Subtitle className={styles.cardTitle}>BI Analyst</Card.Subtitle>
+          <Card.Text className={styles.cardText}>
+            With this path you will learn how to become a great BI scientist.
+          </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  )
+}
 
 export default BlogIndex;
 
