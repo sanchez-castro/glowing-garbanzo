fdecc14d6083b71683d3c5348c49301827388e8e
Jose Smiller <jose.smiller@axented.com>
2020-06-26 20:44:31 -0500

    Refactor post preview component

--- src/components/post-preview.tsx
+++ src/components/post-preview.tsx
@@ -1,112 +1,97 @@
 import React from "react";
+import { Link } from "gatsby";
 import { Container, Row, Col } from "react-bootstrap";
 import styles from "./post-preview.module.scss";
-
-interface PostPreviewProps {
-  post: PostProps;
-  extended?: boolean;
-}
+import PostType from './shared/post-type'
+import Tags from "./shared/post-tags";
 
 interface PostProps {
   title: string;
   date: string;
-  contentPreview: string;
-  postTypes: Array<string>;
-  tags: Array<string>;
-  imagePath: string;
+  excerpt?: string;
+  type?: Array<string>;
+  tags?: Array<string>;
+  featuredImage?: any;
+  link: string;
+  extended?: boolean;
 }
 
-const PostPreview = (props: PostPreviewProps) => {
-  return (
-    props.extended ? 
-    <ExtendedView 
-      title={props.post.title} 
-      date={props.post.date} 
-      contentPreview={props.post.contentPreview} 
-      postTypes={props.post.postTypes}
-      tags={props.post.tags}
-      imagePath={props.post.imagePath}
-    ></ExtendedView> : 
-    <RegularView 
-      title={props.post.title} 
-      date={props.post.date} 
-      contentPreview={props.post.contentPreview} 
-      postTypes={props.post.postTypes}
-      tags={props.post.tags}
-      imagePath={props.post.imagePath}
+const PostPreview = (props: PostProps) => {
+  return props.extended ? (
+    <ExtendedView
+      title={props.title}
+      date={props.date}
+      excerpt={props.excerpt}
+      type={props.type}
+      tags={props.tags}
+      featuredImage={props.featuredImage}
+      link={props.link}
+    ></ExtendedView>
+  ) : (
+    <RegularView
+      title={props.title}
+      date={props.date}
+      excerpt={props.excerpt}
+      type={props.type}
+      tags={props.tags}
+      featuredImage={props.featuredImage}
+      link={props.link}
     ></RegularView>
   );
-};
-
-const checkType = (type: string): string => {
-  switch (type) {
-    case 'Article':
-      return "icon/document.svg";
-    case 'Video':
-      return "icon/video.svg";
-    default:
-      return "icon/document.svg";
-  }
 };
 
 const RegularView = (props: PostProps) => {
   return (
     <Container fluid className={styles.container}>
       <Row className={styles.row}>
-        <Col lg={8} xl={8} className="p-0">
-          <p className={styles.title}>{props.title}</p>
+        <Col className={styles.contentCol}>
+          <Link className={styles.title} to={props.link}>
+            {props.title}
+          </Link>
           <p className={styles.date}>{props.date}</p>
-          <div className={styles.tags}>
-            {props.postTypes.map(postType => (
-              <div className={styles.postType}>
-                <img src={checkType(postType)} alt="post type" />
-                <label>{postType}</label>
-              </div>
-            ))}
-          </div>
-          <p className={styles.contentPreview}>{props.contentPreview}</p>
-          <div className={styles.tags}>
-            {props.tags.map(tag => (
-              <div className={styles.tag}>{tag}</div>
-            ))}
-          </div>
+          { props.type ? <PostType types={props.type}></PostType> : '' }
+          <p className={styles.excerpt}>{props.excerpt}</p>
+          { props.tags ? <Tags tags={props.tags}></Tags> : ''}
         </Col>
-        <Col lg={4} xl={4} className={[styles.imageCol, "p-0"].join(" ")}>
-            <img src={props.imagePath} alt=""/>
-        </Col>
+        {props.featuredImage ? (
+          <Col lg={4} xl={4} className={styles.imageCol}>
+            <Link to={props.link}>
+              <img src={props.featuredImage} alt="" />
+            </Link>
+          </Col>
+        ) : (
+          ""
+        )}
       </Row>
     </Container>
-  )
-}
+  );
+};
 
 const ExtendedView = (props: PostProps) => {
   return (
     <Container fluid className={styles.extendedContainer}>
       <Row className={styles.row}>
-        <Col lg={5} xl={5} className={[styles.imageCol, "p-0"].join(" ")}>
-            <img src={props.imagePath} alt=""/>
-        </Col>
-        <Col lg={7} xl={7} className="p-0">
-          <p className={styles.title}>{props.title}</p>
+        {props.featuredImage ? (
+          <Col lg={5} xl={5} className={styles.imageCol}>
+            <Link to={props.link}>
+              <img src={props.featuredImage} alt="" />
+            </Link>
+          </Col>
+        ) : (
+          ""
+        )}
+        <Col className="p-0">
+          <Link className={styles.title} to={props.link}>
+            {props.title}
+          </Link>
           <p className={styles.date}>{props.date}</p>
-          <div className={styles.tags}>
-            {props.postTypes.map(postType => (
-              <div className={styles.postType}>
-                <img src={checkType(postType)} alt="post type" />
-                <label>{postType}</label>
-              </div>
-            ))}
-          </div>
-          <p className={styles.contentPreview}>{props.contentPreview}</p>
-          <div className={styles.tags}>
-            {props.tags.map(tag => (
-              <div className={styles.tag}>{tag}</div>
-            ))}
-          </div>
+          { props.type ? <PostType types={props.type}></PostType> : '' }
+          <p className={styles.excerpt}>{props.excerpt}</p>
+          { props.tags ? <Tags tags={props.tags}></Tags> : ''}
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
 export default PostPreview;
