b8c8176936fa8596fdf07dafdf9b8b688f9c17fb
Jose Smiller <jose.smiller@axented.com>
2020-06-26 20:48:27 -0500

    Load posts from markdown files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 import PostPreview from '../components/post-preview'
@@ -6,6 +6,10 @@ import SEO from "../components/seo";
 import styles from './index.module.scss'
 import "../global.scss";
 import { Card, Row, Col, Container } from "react-bootstrap";
+import Subscribe from "../components/subscribe";
+import scientistIcon from '../assets/icon/data-scientist.svg'
+import analystIcon from '../assets/icon/data-analyst.svg'
+import biIcon from '../assets/icon/bi-analyst.svg'
 
 interface Props {
   data: {
@@ -19,20 +23,10 @@ interface Props {
 }
 
 const BlogIndex = ({ data }: Props) => {
-  const siteTitle = data.site.siteMetadata.title;
   const posts = data.allMarkdownRemark.edges;
 
-  const mockPost = {
-    title: "Getting and Cleaning Data",
-    date: "June 10, 2020",
-    contentPreview: "In this post you will get an introduction to the main tools and ideas in the data scientist's toolbox. The course gives an overview of the data, questions, and tools that data analysts and data scientists work with…",
-    postTypes: ['Article', 'Video'],
-    tags: ['Data Science', 'AI', 'Business'],
-    imagePath: "https://picsum.photos/500/700"
-  }
-
   return (
-    <Layout location={window.location} title={siteTitle}>
+    <Layout location={window.location}>
       <Container fluid>
         <LearningPaths></LearningPaths>
         <Row className="justify-content-md-center">
@@ -42,37 +36,34 @@ const BlogIndex = ({ data }: Props) => {
                 Explore
               </Col>
             </Row>
-            <PostPreview post={mockPost} extended={true}></PostPreview>
-            <PostPreview post={mockPost}></PostPreview>
-            <PostPreview post={mockPost}></PostPreview>
-            <PostPreview post={mockPost}></PostPreview>
-            <PostPreview post={mockPost}></PostPreview>
+
+            {posts.map(({ node }: any, index: number) => {
+              return (
+                <Fragment>
+                  <PostPreview 
+                    key={node.frontmatter.title}
+                    title={node.frontmatter.title}
+                    date={node.frontmatter.date}
+                    excerpt={node.frontmatter.description || node.excerpt}
+                    type={node.frontmatter.type}
+                    tags={node.frontmatter.tags}
+                    featuredImage={node.frontmatter.featuredImage ? node.frontmatter.featuredImage.childImageSharp.fluid.src : null}
+                    link={node.fields.slug}
+                    extended={index == 0 ? true : false}
+                  ></PostPreview>
+                  {index == 4 ? <Subscribe></Subscribe> : ''}
+                </Fragment>
+              )
+            })}
+
+            <div className={styles.loadPosts}>
+              Load More
+            </div>
+
           </Col>
         </Row>
       </Container>
       <SEO title="All posts" />
-      {/* {posts.map(({ node }: any) => {
-        const title = node.frontmatter.title || node.fields.slug;
-        return (
-          <div key={node.fields.slug}>
-            <h3
-              style={{
-                marginBottom: 1 / 4,
-              }}
-            >
-              <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                {title}
-              </Link>
-            </h3>
-            <small>{node.frontmatter.date}</small>
-            <p
-              dangerouslySetInnerHTML={{
-                __html: node.frontmatter.description || node.excerpt,
-              }}
-            />
-          </div>
-        );
-      })} */}
     </Layout>
   );
 };
@@ -86,7 +77,7 @@ const LearningPaths = () => {
       <Col lg={12} xl={12} className={styles.cards}>
         <Card className={styles.card}>
           <Card.Body>
-          <Card.Title className={styles.cardImage}><img src={"icon/data-scientist.svg"}/></Card.Title>
+          <Card.Title className={styles.cardImage}><img src={scientistIcon}/></Card.Title>
           <Card.Subtitle className={styles.cardTitle}>Data Scientist</Card.Subtitle>
           <Card.Text className={styles.cardText}>
             With this path you will learn how to become a great data scientist.
@@ -96,7 +87,7 @@ const LearningPaths = () => {
 
         <Card className={styles.card}>
           <Card.Body>
-          <Card.Title className={styles.cardImage}><img src={"icon/data-analyst.svg"}/></Card.Title>
+          <Card.Title className={styles.cardImage}><img src={analystIcon}/></Card.Title>
           <Card.Subtitle className={styles.cardTitle}>Data Analyst</Card.Subtitle>
           <Card.Text className={styles.cardText}>
             With this path you will learn how to become a great data analyst.
@@ -106,7 +97,7 @@ const LearningPaths = () => {
 
         <Card className={styles.card}>
           <Card.Body>
-          <Card.Title className={styles.cardImage}><img src={"icon/bi-analyst.svg"}/></Card.Title>
+          <Card.Title className={styles.cardImage}><img src={biIcon}/></Card.Title>
           <Card.Subtitle className={styles.cardTitle}>BI Analyst</Card.Subtitle>
           <Card.Text className={styles.cardText}>
             With this path you will learn how to become a great BI scientist.
@@ -130,14 +121,22 @@ export const pageQuery = graphql`
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
-          excerpt
+          excerpt(pruneLength: 400)
           fields {
             slug
           }
           frontmatter {
             date(formatString: "MMMM DD, YYYY")
             title
-            description
+            tags
+            type
+            featuredImage {
+              childImageSharp {
+                fluid(maxWidth: 800) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
         }
       }
