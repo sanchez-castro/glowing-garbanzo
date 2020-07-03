5f94f9af29d4982d860453d43eab06651d597daa
Jose Smiller <jose.smiller@axented.com>
2020-07-02 19:53:08 -0500

    Add fragment for sharing post query

---
+++ src/shared/fragments/posts.ts
@@ -0,0 +1,29 @@
+import { graphql } from "gatsby";
+
+export const postsFragment = graphql`
+    fragment PostsFragment on Query {
+      allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+        edges {
+          node {
+            excerpt(pruneLength: 400)
+            fields {
+              slug
+            }
+            frontmatter {
+              date(formatString: "MMMM DD, YYYY")
+              title
+              tags
+              type
+              featuredImage {
+                childImageSharp {
+                  fluid(maxWidth: 800) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+`;
