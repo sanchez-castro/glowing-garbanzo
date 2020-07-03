ce9f72d889e2f695d9f9516393c321786f2e30d9
Jose Smiller <jose.smiller@axented.com>
2020-07-02 19:51:04 -0500

    Update layout import

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Layout from "../components/layout";
+import Layout from "../shared/component/layout";
 
 // Components
 import { Helmet } from "react-helmet";
