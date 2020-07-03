01bcc22055d1ba590ec19890e0eb226a59c08d74
Jose Smiller <jose.smiller@axented.com>
2020-07-02 19:50:31 -0500

    Refactor index page components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import React, { Fragment, Component } from "react";
-import { Link, graphql } from "gatsby";
+import React, { Fragment, Component, createRef } from "react";
+import { Link, graphql, navigate } from "gatsby";
 import { Card, Row, Col, Container } from "react-bootstrap";
 import { PostsData } from "../shared/models/post";
-import { IPostsData } from "../shared/interfaces/post";
+import { postsFragment } from '../shared/fragments/posts';
 
 //Components
 import Subscribe from "../shared/component/subscribe";
-import Layout from "../components/layout";
+import Layout from "../shared/component/layout";
 import PostPreview from "../shared/component/post-preview";
 import SEO from "../components/seo";
 import ExtendedSearchbar from "../shared/component/extended-searchbar";
@@ -19,6 +19,7 @@ import biIcon from "../assets/icon/bi-analyst.svg";
 //Styles
 import styles from "./index.module.scss";
 import "../global.scss";
+import PostList from "../shared/component/post-list";
 
 interface IndexProps {
   data: {
@@ -33,52 +34,44 @@ interface IndexProps {
 
 interface IndexState {
   postsData: PostsData;
-  searchTerm: string;
 }
 
 class BlogIndex extends Component<IndexProps, IndexState> {
+  selectedTags: Array<string> = []
   constructor(props: IndexProps) {
     super(props);
     this.state = {
-      postsData: new PostsData(props.data),
-      searchTerm: ""
+      postsData: new PostsData(props.data)
     };
   }
 
-  searchInputHandler(event: React.ChangeEvent<HTMLInputElement>) {
-    const query = event.target.value.toLowerCase();
-    const dummyData = {...this.state.postsData};
-    const posts = dummyData.allMarkdownRemark.edges || [];
-    const filteredData = posts.filter((post: any) => {
-      let { description, title, tags } = post.node.frontmatter;
-      description = description ? description.toLowerCase() : "";
-      title = title ? title.toLowerCase() : "";
-      return (
-        description.toLowerCase().includes(query) ||
-        title.toLowerCase().includes(query) ||
-        (tags && tags.join("").toLowerCase().includes(query))
-      );
-    });
-    dummyData.allMarkdownRemark.edges = filteredData
-    this.setState(
-      {
-        postsData: dummyData,
-        searchTerm: query
-      }
-    )
+  searchInputHandler(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter') {
+      const target = event.target as HTMLInputElement
+      navigate(
+        "/search-result/",
+        {
+          state: { searchTerm: target.value, tags: this.selectedTags },
+        }
+      )
+    }
   }
 
+  tagSelectHandler = (tags: Array<string>) => {
+    this.selectedTags = tags
+  }  
+
   render() {
-    console.log(this.state);
     return (
       <Layout location={window.location}>
         <Container fluid>
           <ExtendedSearchbar
-            onChange={this.searchInputHandler.bind(this)}
+            onTagSelected={this.tagSelectHandler}
+            onKeyDown={this.searchInputHandler.bind(this)}
           ></ExtendedSearchbar>
           <LearningPaths></LearningPaths>
           <Row className="justify-content-md-center">
-            <Col lg={12} xl={12}>
+            <Col lg={10} xl={10}>
               <Row className={styles.explore}>
                 <Col className={styles.title}>Explore</Col>
               </Row>
@@ -92,58 +85,15 @@ class BlogIndex extends Component<IndexProps, IndexState> {
   }
 }
 
-interface PostListProps {
-  list: PostsData;
-}
-
-const PostList = (props: PostListProps) => {
-  const posts = props.list.allMarkdownRemark.edges;
-  return (
-    <Fragment>
-      {posts.map(({ node }: any, index: number) => {
-        const {
-          title,
-          date,
-          description,
-          type,
-          tags,
-          featuredImage,
-          slug
-        } = node.frontmatter;
-        return (
-          <Fragment>
-            <PostPreview
-              key={slug}
-              title={title}
-              date={date}
-              excerpt={description || node.excerpt}
-              type={type}
-              tags={tags}
-              featuredImage={
-                featuredImage ? featuredImage.childImageSharp.fluid.src : null
-              }
-              link={slug}
-              extended={index == 0 ? true : false}
-            ></PostPreview>
-            {index == 4 ? <Subscribe></Subscribe> : ""}
-          </Fragment>
-        );
-      })}
-
-      <div className={styles.loadPosts}>Load More</div>
-    </Fragment>
-  );
-};
-
 const LearningPaths = () => {
   return (
     <Row
       className={["justify-content-md-center", styles.learningPaths].join(" ")}
     >
-      <Col lg={12} xl={12} className={styles.title}>
+      <Col lg={10} xl={10} className={styles.title}>
         Learning paths
       </Col>
-      <Col lg={12} xl={12} className={styles.cards}>
+      <Col lg={10} xl={10} className={styles.cards}>
         <Card className={styles.card}>
           <Card.Body>
             <Card.Title className={styles.cardImage}>
@@ -194,29 +144,7 @@ const LearningPaths = () => {
 export default BlogIndex;
 
 export const pageQuery = graphql`
-  query Posts {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            tags
-            type
-            featuredImage {
-              childImageSharp {
-                fluid(maxWidth: 800) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+  query {
+    ...PostsFragment
   }
 `;
