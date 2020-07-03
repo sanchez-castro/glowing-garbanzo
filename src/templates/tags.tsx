6eee4b3e5288a5ce8f71543c5417bcf8017438a9
Jose Smiller <jose.smiller@axented.com>
2020-07-02 19:54:26 -0500

    Update layout import

--- src/templates/tags.tsx
+++ src/templates/tags.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link, graphql, navigate } from "gatsby";
-import Layout from "../components/layout";
+import Layout from "../shared/component/layout";
 import PostPreview from "../shared/component/post-preview";
 import { Row, Col, Container } from "react-bootstrap";
 import styles from './tags.module.scss'
