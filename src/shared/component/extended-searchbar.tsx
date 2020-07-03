29c6eb27266a2f7d690f89e6b47e481040cb8ffb
Jose Smiller <jose.smiller@axented.com>
2020-07-02 19:52:05 -0500

    Update searchbar component to get tags from md files

--- src/shared/component/extended-searchbar.tsx
+++ src/shared/component/extended-searchbar.tsx
@@ -1,90 +1,101 @@
-import React, { useState, Fragment } from 'react'
-import { Row, Col } from 'react-bootstrap';
-import styles from './extended-searchbar.module.scss'
-import searchIcon from '../../assets/icon/search.svg'
-import { useStaticQuery, graphql } from 'gatsby';
+import React, { useState, Fragment } from "react";
+import { Row, Col } from "react-bootstrap";
+import styles from "./extended-searchbar.module.scss";
+import searchIcon from "../../assets/icon/search.svg";
+import { useStaticQuery, graphql } from "gatsby";
+
+interface SearchbarState {
+  selectedTags: Array<string>
+}
 
 const ExtendedSearchbar = (props: any) => {
-  const emptyQuery = ""
-  const [state, setState] = useState({
-    filteredData: [],
-    query: emptyQuery,
-  })
+  const [state, setState] = useState<SearchbarState>({
+    selectedTags: props.selectedTags ? props.selectedTags : [""]
+  });
 
-  const { tagss } = useStaticQuery(
+  const data = useStaticQuery(
     graphql`
-    query Tags{
+      query Tags {
         allMarkdownRemark(limit: 2000) {
           group(field: frontmatter___tags) {
             fieldValue
-            totalCount
           }
         }
       }
     `
   );
 
-  console.log(tagss)
-  
-  const tags = [
-    "Data Science",
-    "Machine Learning",
-    "Programming",
-    "AI",
-    "Data Science",
-    "Business Intelligence",
-    "Knowledge",
-    "Business",
-    "Culture"
-  ];
+  const tags: Array<Tag> = data.allMarkdownRemark.group.map((tag: any) => {
+    return { 
+      title: tag.fieldValue, 
+      active: state.selectedTags.includes(tag.fieldValue) 
+    };
+  });
+
+  const addTag = (title: string) => {
+    let selectedTags = state.selectedTags;
+    if (selectedTags.includes(title)) {
+      selectedTags = selectedTags.filter((tag: string) => tag !== title);
+    } else {
+      selectedTags.push(title);
+    }
+    setState({ selectedTags: selectedTags });
+    if (props.onTagSelected) {
+      props.onTagSelected(selectedTags);
+    }
+  };
 
   return (
-    <Fragment>
+    <div>
       <Row className="justify-content-md-center">
         <Col className="d-flex justify-content-center" lg={10} xl={10}>
           <div className={styles.extendedInputContainer}>
-            <input 
+            <input
               className={styles.extendedSearchbar}
               type="text"
               placeholder="What do you want to learn today?"
-              onChange={props.onChange}
+              onKeyDown={props.onKeyDown}
             />
-            <img className={styles.inputIcon} src={searchIcon} alt=""/>
+            <img className={styles.inputIcon} src={searchIcon} alt="" />
           </div>
         </Col>
       </Row>
       <Row className="justify-content-md-center">
-          <small>Filter by tag</small>
+        <small>Filter by tag</small>
       </Row>
       <Row className="justify-content-md-center">
         <Col className="d-flex justify-content-center" lg={7} xl={7}>
           <div className={styles.tags}>
-            {tags.map(tag => (
-              <Tag title={tag}></Tag>
+            {tags.map((tag: Tag) => (
+              <div
+                className={styles.tagContainer}
+                onClick={() => addTag(tag.title)}
+              >
+                <Tag title={tag.title} active={tag.active}></Tag>
+              </div>
             ))}
           </div>
         </Col>
       </Row>
       <Row className={["justify-content-md-center", styles.moreTags].join(" ")}>
-          <small>See all tags</small>
+        <small>See all tags</small>
       </Row>
-    </Fragment>
+    </div>
   );
 };
 
-interface TagProps {
-  title: string;
+interface Tag {
+  title: string
+  active: boolean
 }
 
-const Tag = (props: TagProps) => {
-  const [select, setSelected] = useState(false);
+const Tag = (props: Tag) => {
   return (
     <button
-      onClick={() => setSelected(!select)}
-      className={[select ? styles.activeTag : "", styles.tag].join(" ")}
+      className={[props.active ? styles.activeTag : "", styles.tag].join(" ")}
     >
       {props.title}
-      {select ? (
+      {props.active ? (
         <label className={styles.deleteTag}>
           <small>&#10005;</small>
         </label>
@@ -97,4 +108,4 @@ const Tag = (props: TagProps) => {
   );
 };
 
-export default ExtendedSearchbar
+export default ExtendedSearchbar;
