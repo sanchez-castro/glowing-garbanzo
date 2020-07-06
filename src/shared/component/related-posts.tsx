349d69e088e8b98a9b188ce7e4cb2021fce51bf7
Jose Smiller <jose.smiller@axented.com>
2020-07-06 18:34:57 -0500

    Get a list of related posts using tags and title

--- src/shared/component/related-posts.tsx
+++ src/shared/component/related-posts.tsx
@@ -1,51 +1,81 @@
-import React from 'react'
-import PostPreview from './post-preview';
+import React, { Fragment } from "react";
+import PostPreview from "./post-preview";
 import { useStaticQuery, graphql } from "gatsby";
+import { PostsData } from "../models/post";
+import Fuse from "fuse.js";
+import _ from "lodash";
 
 interface Props {
-    //tags: Array<string>
+  title: string;
+  tags: string[];
+  parentId: string
 }
 
-const RelatedPosts = (tags: Props) => {
-    const { site } = useStaticQuery(
-        graphql`
-        query {
-            site {
-                siteMetadata {
-                    title
-                }   
-            }
-            allMarkdownRemark(limit: 1000, sort: { fields: [frontmatter___date], order: DESC }) {
-              edges {
-                node {
-                  excerpt(pruneLength: 400)
-                  fields {
-                    slug
-                  }
-                  frontmatter {
-                    date(formatString: "MMMM DD, YYYY")
-                    title
-                    tags
-                    type
-                    featuredImage {
-                      childImageSharp {
-                        fluid(maxWidth: 800) {
-                          ...GatsbyImageSharpFluid
-                        }
-                      }
-                    }
-                  }
-                }
+const RelatedPosts = (props: Props) => {
+  const postsQuery = useStaticQuery(
+    graphql`
+      query {
+        ...PostsFragment
+      }
+    `
+  );
+
+  const posts = new PostsData(postsQuery);
+  const options = {
+    includeScore: true,
+    keys: ["node.excerpt", "node.frontmatter.title", "node.frontmatter.tags"]
+  };
+
+  const fuse = new Fuse(posts.allMarkdownRemark.edges, options);
+  const tagsResult = fuse.search(props.tags.join(" "));
+  const titleResult = fuse.search(props.title);
+
+  const result = tagsResult.concat(titleResult).sort(function (a, b) {
+    var keyA = a.score || 0, keyB = b.score || 0;
+    if (keyA < keyB) return -1;
+    if (keyA > keyB) return 1;
+    return 0;
+  });
+
+  const reducedArray = _.uniq(result
+                        .map(result => result.item)
+                        .filter( item => item.node.id !== props.parentId))
+                        .slice(0, 3);
+  posts.allMarkdownRemark.edges = reducedArray;
+
+  return (
+    <Fragment>
+      <h2 style={{ margin: "10vh 0 5vh 0" }}>Related articles</h2>
+      {posts.allMarkdownRemark.edges.map(({ node }: any) => {
+        const {
+          title,
+          date,
+          description,
+          type,
+          tags,
+          featuredImage
+        } = node.frontmatter;
+        const slug = node.fields.slug;
+        return (
+          <Fragment>
+            <PostPreview
+              key={slug}
+              title={title}
+              date={date}
+              excerpt={description || node.excerpt}
+              type={type}
+              tags={tags}
+              featuredImage={
+                featuredImage ? featuredImage.childImageSharp.fluid.src : null
               }
-            }
-          }
-        `
-      );
+              link={slug}
+              extended={false}
+            ></PostPreview>
+          </Fragment>
+        );
+      })}
+    </Fragment>
+  );
+};
 
-      console.log(site)
-      return (
-          <div></div>
-      )
-}
-
-export default RelatedPosts
+export default RelatedPosts;
