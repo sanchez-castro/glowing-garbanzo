00ccc3aa8701e9660c93c473b1538b59632c70b2
Jose Smiller <jose.smiller@axented.com>
2020-07-06 18:28:40 -0500

    Add subscribe function to footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "gatsby";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Toast } from "react-bootstrap";
 import styles from './footer.module.scss';
 import twitterIcon from '../assets/icon/twitter.svg'
 import facebookIcon from '../assets/icon/facebook.svg'
 import instagramIcon from '../assets/icon/instagram.svg'
+import addToMailchimp from 'gatsby-plugin-mailchimp'
+
+interface SubscribeState {
+    email: string;
+    result: any;
+}
 
 const Footer = () => {
+    const [state, setState] = useState<SubscribeState>({
+        email: "", result: null
+      });
+    const handleChange = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        const target = event.target as HTMLInputElement
+        const email = target.value ? target.value : ''
+        setState({ email: email, result: null })
+        if (event.key === 'Enter') {
+            handleSubmit()
+        }
+    }
+    const handleSubmit = async () => {
+        const result = await addToMailchimp(state.email)
+        console.log(result)
+        setState({ email: state.email, result: result})
+    }
     return(
         <footer className={styles.footer}>
             <Container className={styles.container} fluid>
@@ -62,10 +84,18 @@ const Footer = () => {
                     </Col>
                     <Col className={styles.subscribe}>
                         <p className={styles.strong}>Subscribe</p>
-                        <input className={styles.email} placeholder="Your email address" type="text"/>
+                        <input className={styles.email} placeholder="Your email address" type="text" onKeyUp={handleChange}/>
                     </Col>
                 </Row>
             </Container>
+            <Toast delay={3000} autohide show={state.result} onClose={() => setState({ email: state.email, result: null })} className={styles.toast}>
+                <Toast.Header className={styles.toastHeader}>
+                    <strong className="mr-auto">Data Product Design</strong>
+                </Toast.Header>
+                <Toast.Body>
+                    { state.result ? <div dangerouslySetInnerHTML={{ __html: state.result.msg }} /> : '' }
+                </Toast.Body>
+            </Toast>
         </footer>
     )
 }
