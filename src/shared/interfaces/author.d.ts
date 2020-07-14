4f5a460a0e70c06c212686e09c647626be0d04c0
Jose Smiller <jose.smiller@axented.com>
2020-07-13 19:18:31 -0500

    Add author interface

---
+++ src/shared/interfaces/author.d.ts
@@ -0,0 +1,17 @@
+import { IChildImageSharp } from "./image";
+
+export interface IAuthor {
+  name: string;
+  bio: string;
+  title: string;
+  location?: string;
+  id: string;
+  twitter?: string;
+  github?: string;
+  avatar?: string;
+  image?: IAuthorImage
+}
+
+export interface IAuthorImage {
+  childImageSharp: IChildImageSharp
+}
