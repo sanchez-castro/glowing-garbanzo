4a3e9c2db82ca843d428a57cf6197d2b07747db3
Jose Smiller <jose.smiller@axented.com>
2020-07-21 12:32:48 -0500

    Add responsiveness to about us page

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -4,18 +4,26 @@ import { Container, Row, Col } from "react-bootstrap";
 import styles from "./about-us.module.scss";
 
 //assets
-import user from "../assets/image/philosophy-banner.jpg";
 import { graphql } from "gatsby";
 import { Author } from "../shared/models/author";
-import { ChildImageSharp } from '../shared/models/image';
+import { ChildImageSharp } from "../shared/models/image";
+import title from "../assets/icon/about-title.svg";
+import email from "../assets/icon/about-email.svg";
+import github from "../assets/icon/about-github.svg";
+import location from "../assets/icon/about-location.svg";
+import twitter from "../assets/icon/twitter.svg";
 
 interface Props {
   data: any;
 }
 
 const AboutUs = (props: Props) => {
-  const authors = props.data.allAuthorYaml.edges.map( (edge: any) => new Author(edge.node))
-  const founder = authors.find((author: Author) => author.id == "adrian@ianalytics.org")
+  const authors = props.data.allAuthorYaml.edges.map(
+    (edge: any) => new Author(edge.node)
+  );
+  const founder = authors.find(
+    (author: Author) => author.id == "adrian@ianalytics.org"
+  );
   const collaborators = authors.filter(
     (author: Author) => author.id !== "adrian@ianalytics.org"
   );
@@ -24,16 +32,38 @@ const AboutUs = (props: Props) => {
     <Layout>
       <Container fluid>
         <Row className="justify-content-md-center">
-          <Col lg={10} className={styles.header}>
-            <p className="headline-1">About us</p>
-            <p className="headline-3">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+          <Col
+            lg={10}
+            className={[styles.header, "d-block d-lg-none"].join(" ")}
+          >
+            <p className="headline-1 text-center">Sobre nosotros</p>
+            <p className="headline-3 text-center">
+              Tu lugar para aprender Ciencia de Datos en Español
             </p>
           </Col>
-          <Col lg={8}>
+          <Col
+            lg={10}
+            className={[styles.header, "d-none d-lg-block"].join(" ")}
+          >
+            <p className="headline-1">Sobre nosotros</p>
+            <p className="headline-3">
+              Tu lugar para aprender Ciencia de Datos en Español
+            </p>
+          </Col>
+          <Col lg={8} className="d-block d-lg-none">
+            <About
+              name={founder.name}
+              bio={founder.bio}
+              title={founder.title}
+              location={founder.location}
+              email={founder.id}
+              twitter={founder.twitter}
+              github={founder.github}
+              image={founder.image}
+              mobile={true}
+            ></About>
+          </Col>
+          <Col lg={8} className="d-none d-lg-block">
             <About
               name={founder.name}
               bio={founder.bio}
@@ -50,12 +80,37 @@ const AboutUs = (props: Props) => {
           </Col>
           {collaborators.length > 0 ? (
             <Fragment>
-              <Col lg={10} className={styles.collaborators}>
-                <p className="headline-1">Collaborators</p>
+              <Col lg={10} className="d-block d-lg-none text-center">
+                <p className="headline-1">Colaboradores</p>
               </Col>
-              <Col lg={8}>
-                {collaborators.map( (collaborator: any) => (
-                    <About
+              <Col
+                lg={10}
+                className={[styles.collaborators, "d-none d-lg-block"].join(
+                  " "
+                )}
+              >
+                <p className="headline-1">Colaboradores</p>
+              </Col>
+              <Col lg={8} className="d-block d-lg-none">
+                {collaborators.map((collaborator: any) => (
+                  <About
+                    key={collaborator.name}
+                    name={collaborator.name}
+                    bio={collaborator.bio}
+                    title={collaborator.title}
+                    location={collaborator.location}
+                    email={collaborator.id}
+                    twitter={collaborator.twitter}
+                    github={collaborator.github}
+                    image={collaborator.image}
+                    mobile={true}
+                  ></About>
+                ))}
+              </Col>
+              <Col sm={12} className="d-none d-lg-block">
+                {collaborators.map((collaborator: any) => (
+                  <About
+                    key={collaborator.name}
                     name={collaborator.name}
                     bio={collaborator.bio}
                     title={collaborator.title}
@@ -86,39 +141,123 @@ interface AboutProps {
   twitter?: string;
   github?: string;
   avatar?: string;
-  image?: any
+  image?: any;
+  mobile?: true;
 }
 
 const About = (props: AboutProps) => {
   return (
-    <Container className={styles.about}>
+    <Container
+      className={[styles.about, props.mobile ? styles.aboutMobile : ""].join(
+        " "
+      )}
+    >
       <Row className="justify-content-md-center">
-        <Col lg={3} className={styles.imageCol}>
-          {props.image ? <img src={props.image.childImageSharp.fluid.src} alt="" /> : ''}
+        <Col
+          xs={12}
+          sm={12}
+          md={3}
+          lg={3}
+          xl={3}
+          className={[
+            styles.imageCol,
+            props.mobile ? "justify-content-center" : "",
+          ].join(" ")}
+        >
+          {props.image ? (
+            <img
+              className={[
+                styles.avatar,
+                props.mobile ? styles.mobileAvatar : "",
+              ].join(" ")}
+              src={props.image.childImageSharp.fluid.src}
+              alt=""
+            />
+          ) : (
+            ""
+          )}
         </Col>
-        <Col lg={9} className={styles.contentCol}>
-          <p className="headline-3">
+        <Col lg={9} className={props.mobile ? "" : styles.contentCol}>
+          <p
+            className={[
+              "headline-3 m-0",
+              props.mobile ? "text-center" : "",
+            ].join(" ")}
+          >
             {props.name}{" "}
-            <label className={[styles.twitterHandle, "headline-4"].join(" ")}>
-              {props.twitter}
-            </label>
+            {!props.mobile ? (
+              <label className={[styles.twitterHandle, "headline-4"].join(" ")}>
+                <a
+                  href={"https://www.twitter.com/" + props.twitter}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  @{props.twitter}
+                </a>
+              </label>
+            ) : (
+              ""
+            )}
           </p>
+          {props.mobile ? (
+            <p className="text-center m-0">
+              {" "}
+              <label className={[styles.twitterHandle, "headline-4"].join(" ")}>
+                <a
+                  href={"https://www.twitter.com/" + props.twitter}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  @{props.twitter}
+                </a>
+              </label>
+            </p>
+          ) : (
+            ""
+          )}
           <p className="paragraph">{props.bio}</p>
-          <div className={styles.title}>
-            <label className="small-text">{props.title}</label>
-          </div>
-          <div className={styles.location}>
-            <label className="small-text">{props.location}</label>
-          </div>
-          <div className={styles.email}>
-            <label className="small-text">{props.email}</label>
-          </div>
-          <div className={styles.twitter}>
-            <label className="small-text">{props.twitter}</label>
-          </div>
-          <div className={styles.github}>
-            <label className="small-text">{props.github}</label>
-          </div>
+          <Container>
+            <Row className="m-0">
+              <Col className={[styles.logo, "p-0"].join(" ")} xs={1}>
+                <img src={title} alt="title" />
+              </Col>
+              <Col className="p-0" xs={11}>
+                <label className="small-text">{props.title}</label>
+              </Col>
+            </Row>
+            <Row className="m-0">
+              <Col className={[styles.logo, "p-0"].join(" ")} xs={1}>
+                <img src={location} alt="title" />
+              </Col>
+              <Col className="p-0" xs={11}>
+                <label className="small-text">{props.location}</label>
+              </Col>
+            </Row>
+            <Row className="m-0">
+              <Col className={[styles.logo, "p-0"].join(" ")} xs={1}>
+                <img src={email} alt="title" />
+              </Col>
+              <Col className="p-0" xs={11}>
+                <label className="small-text">{props.email}</label>
+              </Col>
+            </Row>
+            <Row className="m-0">
+              <Col className={[styles.logo, "p-0"].join(" ")} xs={1}>
+                <img src={twitter} alt="title" />
+              </Col>
+              <Col className="p-0" xs={11}>
+                <label className="small-text">{props.twitter}</label>
+              </Col>
+            </Row>
+            <Row className="m-0">
+              <Col className={[styles.logo, "p-0"].join(" ")} xs={1}>
+                <img src={github} alt="title" />
+              </Col>
+              <Col className="p-0" xs={11}>
+                <label className="small-text">{props.github}</label>
+              </Col>
+            </Row>
+          </Container>
         </Col>
       </Row>
     </Container>
