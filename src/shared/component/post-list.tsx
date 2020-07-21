6964e1961159801fc4ed90816d5389a1b9194272
Jose Smiller <jose.smiller@axented.com>
2020-07-21 12:40:58 -0500

    Add responsiveness to post list component

--- src/shared/component/post-list.tsx
+++ src/shared/component/post-list.tsx
@@ -1,96 +1,112 @@
-import React, { Fragment, Component } from 'react'
-import { PostsData } from '../models/post';
-import PostPreview from './post-preview';
-import Subscribe from './subscribe';
-import styles from './post-list.module.scss'
-import _ from 'lodash'
-import { Edge } from '../models/post';
-
+import React, { Fragment, Component } from "react";
+import { PostsData } from "../models/post";
+import PostPreview from "./post-preview";
+import Subscribe from "./subscribe";
+import styles from "./post-list.module.scss";
+import _ from "lodash";
+import { Edge } from "../models/post";
+import { Col, Row } from "react-bootstrap";
 
 interface PostListProps {
-    list: PostsData;
-    result?: boolean;
-}  
+  list: PostsData;
+  result?: boolean;
+  mobile?: boolean;
+}
 
 interface PostListState {
-  visiblePosts: Edge[],
-  fullList: boolean
+  visiblePosts: Edge[];
+  fullList: boolean;
 }
 
 class PostList extends Component<PostListProps, PostListState> {
-    private postList: PostsData
-    private resultList: boolean
-    constructor(props: PostListProps) {
-      super(props)
-      this.postList = _.cloneDeep(props.list)
-      this.resultList = props.result ? props.result : false
-      this.state = {
+  private postList: PostsData;
+  private resultList: boolean;
+  constructor(props: PostListProps) {
+    super(props);
+    this.postList = _.cloneDeep(props.list);
+    this.resultList = props.result ? props.result : false;
+    this.state = {
+      visiblePosts: [],
+      fullList: false,
+    };
+  }
+
+  componentDidMount() {
+    this.loadMore();
+  }
+
+  componentWillUpdate() {
+    this.postList = _.cloneDeep(this.props.list);
+
+    this.setState(
+      {
         visiblePosts: [],
-        fullList: false
-      }
-    }
+        fullList: false,
+      },
+      () => this.loadMore()
+    );
+  }
 
-    componentDidMount() {
-      this.loadMore()
-    }
+  loadMore() {
+    const listLength = this.state.visiblePosts.length;
 
-    componentWillReceiveProps(props: PostListProps) {
-      this.postList = _.cloneDeep(props.list)
+    this.setState({
+      visiblePosts: this.postList.allMarkdownRemark.edges.slice(
+        0,
+        listLength + 6
+      ),
+      fullList: listLength == this.postList.allMarkdownRemark.edges.length,
+    });
+  }
 
-      this.setState({
-        visiblePosts: [],
-        fullList: false
-      }, () => this.loadMore())
-    }
+  render() {
+    return (
+      <Fragment>
+        {this.state.visiblePosts.map(({ node }: any, index: number) => {
+          const {
+            title,
+            date,
+            description,
+            contentType,
+            tags,
+            featuredImage,
+          } = node.frontmatter;
+          const slug = node.fields.slug;
+          return (
+            <Fragment key={index}>
+              <PostPreview
+                key={slug}
+                title={title}
+                date={date}
+                excerpt={description || node.excerpt}
+                contentType={contentType}
+                tags={tags}
+                featuredImage={
+                  featuredImage ? featuredImage.childImageSharp.fluid.src : null
+                }
+                link={slug}
+                extended={index == 0 && !this.resultList ? true : false}
+                mobile={this.props.mobile}
+              ></PostPreview>
+              {index == 4 && !this.resultList ? <Subscribe></Subscribe> : ""}
+            </Fragment>
+          );
+        })}
 
-    loadMore() {
-      const listLength = this.state.visiblePosts.length
+        {this.state.fullList ? (
+          ""
+        ) : (
+          <Row className="justify-content-center">
+            <Col xs={10} sm={10} md={10} lg={12}>
+              <div className={styles.loadPosts} onClick={() => this.loadMore()}>
+                Cargar más
+              </div>
+            </Col>
+          </Row>
+        )}
+      </Fragment>
+    );
+  }
+}
 
-      this.setState(
-        {
-          visiblePosts: this.postList.allMarkdownRemark.edges.slice(0, listLength + 6),
-          fullList: listLength == this.postList.allMarkdownRemark.edges.length
-        }
-      )
-    }
-
-    render() {
-      return (
-        <Fragment>
-          {this.state.visiblePosts.map(({ node }: any, index: number) => {
-            const {
-              title,
-              date,
-              description,
-              contentType,
-              tags,
-              featuredImage
-            } = node.frontmatter;
-            const slug = node.fields.slug
-            return (
-              <Fragment>
-                <PostPreview
-                  key={slug}
-                  title={title}
-                  date={date}
-                  excerpt={description || node.excerpt}
-                  contentType={contentType}
-                  tags={tags}
-                  featuredImage={
-                    featuredImage ? featuredImage.childImageSharp.fluid.src : null
-                  }
-                  link={slug}
-                  extended={index == 0 && !this.resultList ? true : false}
-                ></PostPreview>
-                {index == 4 && !this.resultList ? <Subscribe></Subscribe> : ""}
-              </Fragment>
-            );
-          })}
-    
-          { this.state.fullList ? '' : <div className={styles.loadPosts} onClick={() => this.loadMore()}>Load More</div>}
-        </Fragment>
-      );
-    }
-  };
-
-  export default PostList
+export default PostList;
