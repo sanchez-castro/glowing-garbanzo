41f300740cd252ed60e52e3a52e85cd7f666912c
Jose Smiller <jose.smiller@axented.com>
2020-07-21 12:49:25 -0500

    Add responsiveness to subscribe component

--- src/shared/component/subscribe.tsx
+++ src/shared/component/subscribe.tsx
@@ -1,59 +1,130 @@
-import React, { useState } from 'react'
-import { Container, Row, Col, Toast } from 'react-bootstrap'
-import styles from './subscribe.module.scss'
-import arrowIcon from '../../assets/icon/right-arrow.svg'
-import addToMailchimp from 'gatsby-plugin-mailchimp'
+import React, { useState, Fragment } from "react";
+import { Container, Row, Col, Toast } from "react-bootstrap";
+import styles from "./subscribe.module.scss";
+import arrowIcon from "../../assets/icon/right-arrow.svg";
+import addToMailchimp from "gatsby-plugin-mailchimp";
 
 interface SubscribeState {
-    email: string;
-    result: any;
+  email: string;
+  result: any;
+  displayToast: boolean;
 }
 
 const Subscribe = () => {
-    const [state, setState] = useState<SubscribeState>({
-        email: "", result: null
-      });
-    const handleChange = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        const target = event.target as HTMLInputElement
-        const email = target.value ? target.value : ''
-        setState({ email: email, result: null })
-        if (event.key === 'Enter') {
-            handleSubmit()
-        }
+  const [state, setState] = useState<SubscribeState>({
+    email: "",
+    result: null,
+    displayToast: false,
+  });
+  const handleChange = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    const email = target.value ? target.value : "";
+    setState({ email: email, result: null, displayToast: false });
+    if (event.key === "Enter") {
+      handleSubmit();
     }
-    const handleSubmit = async () => {
-        const result = await addToMailchimp(state.email)
-        console.log(result)
-        setState({ email: state.email, result: result})
-    }
-    return(
-        <Container fluid className={styles.container}>
-            <Row className={styles.row}>
-                <Col lg={6}>
-                    <p className={styles.title}>
-                        Subscribe to our insights
-                    </p>
-                    <p className={styles.subtitle}>
-                        You’ll get the latest and greatest news, articles and posts.
-                    </p>
-                </Col>
-                <Col lg={6}>
-                    <div className={styles.inputContainer}>
-                        <input placeholder="Your email address" className={styles.email} type="text" onKeyUp={handleChange}/>
-                        <img className={styles.inputIcon} src={arrowIcon} alt=""/>
-                    </div>
-                </Col>
-                <Toast delay={3000} autohide show={state.result} onClose={() => setState({ email: state.email, result: null })} className={styles.toast}>
-                    <Toast.Header className={styles.toastHeader}>
-                        <strong className="mr-auto">Data Product Design</strong>
-                    </Toast.Header>
-                    <Toast.Body>
-                        { state.result ? <div dangerouslySetInnerHTML={{ __html: state.result.msg }} /> : '' }
-                    </Toast.Body>
-                </Toast>
-            </Row>
-        </Container>
-    )
-}
+  };
+  const handleSubmit = async () => {
+    const result = await addToMailchimp(state.email);
+    setState({ email: state.email, result: result, displayToast: true });
+  };
+  return (
+    <Fragment>
+      <Container
+        fluid
+        className={[styles.container, "d-none d-lg-block"].join(" ")}
+      >
+        <Landscape handleChange={handleChange}></Landscape>
+      </Container>
 
-export default Subscribe
+      <Container
+        fluid
+        className={[
+          styles.container,
+          styles.mobileContainer,
+          "d-block d-lg-none",
+        ].join(" ")}
+      >
+        <Mobile handleChange={handleChange}></Mobile>
+      </Container>
+
+      <Toast
+        delay={3000}
+        autohide
+        show={state.result}
+        onClose={() =>
+          setState({
+            email: state.email,
+            result: state.result,
+            displayToast: false,
+          })
+        }
+      >
+        <Toast.Header className={styles.toastHeader}>
+          <strong className="mr-auto">Data Product Design</strong>
+        </Toast.Header>
+        <Toast.Body>
+          {state.result ? (
+            <div dangerouslySetInnerHTML={{ __html: state.result.msg }} />
+          ) : (
+            ""
+          )}
+        </Toast.Body>
+      </Toast>
+    </Fragment>
+  );
+};
+
+const Mobile = (props: any) => {
+  return (
+    <Row className={styles.row}>
+      <Col lg={6}>
+        <p className={[styles.title, "headline-3"].join(" ")}>
+          Suscríbete a nuestras ideas
+        </p>
+        <p className={[styles.title, "paragraph"].join(" ")}>
+          Obtendrás las últimas y mejores noticias, artículos y publicaciones.
+        </p>
+      </Col>
+      <Col lg={6}>
+        <div className={styles.inputContainer}>
+          <input
+            placeholder="Tu dirección de correo"
+            className={styles.email}
+            type="text"
+            onKeyUp={props.handleChange}
+          />
+          <img className={styles.inputIcon} src={arrowIcon} alt="" />
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+const Landscape = (props: any) => {
+  return (
+    <Row className={styles.row}>
+      <Col lg={6}>
+        <p className={[styles.title, "headline-3"].join(" ")}>
+          Suscríbete a nuestras ideas
+        </p>
+        <p className={[styles.title, "paragraph"].join(" ")}>
+          Obtendrás las últimas y mejores noticias, artículos y publicaciones.
+        </p>
+      </Col>
+      <Col lg={6}>
+        <div className={styles.inputContainer}>
+          <input
+            placeholder="Tu dirección de correo"
+            className={styles.email}
+            type="text"
+            onKeyUp={props.handleChange}
+          />
+          <img className={styles.inputIcon} src={arrowIcon} alt="" />
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+export default Subscribe;
