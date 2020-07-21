47fb324be6994613cc528da64be5584b4720b800
Jose Smiller <jose.smiller@axented.com>
2020-07-21 12:51:02 -0500

    Add lint corrections

--- src/shared/models/author.ts
+++ src/shared/models/author.ts
@@ -1,5 +1,5 @@
 import { IAuthor, IAuthorImage } from "../interfaces/author";
-import { ChildImageSharp } from './image';
+import { ChildImageSharp } from "./image";
 
 export class Author {
   name: string;
@@ -12,21 +12,21 @@ export class Author {
   avatar: string;
   image: AuthorImage | null;
   constructor(attrs: IAuthor) {
-    this.name = attrs.name
-    this.bio = attrs.bio
-    this.title = attrs.title
-    this.location = attrs.location ? attrs.location : ''
-    this.id = attrs.id
-    this.twitter = attrs.twitter ? attrs.twitter : ''
-    this.github = attrs.github ? attrs.github : ''
-    this.avatar = attrs.avatar ? attrs.avatar : ''
-    this.image = attrs.image ? new AuthorImage(attrs.image) : null
+    this.name = attrs.name;
+    this.bio = attrs.bio;
+    this.title = attrs.title ? attrs.title : "";
+    this.location = attrs.location ? attrs.location : "";
+    this.id = attrs.id;
+    this.twitter = attrs.twitter ? attrs.twitter : "";
+    this.github = attrs.github ? attrs.github : "";
+    this.avatar = attrs.avatar ? attrs.avatar : "";
+    this.image = attrs.image ? new AuthorImage(attrs.image) : null;
   }
 }
 
 class AuthorImage {
-  childImageSharp: ChildImageSharp
+  childImageSharp: ChildImageSharp;
   constructor(attrs: IAuthorImage) {
-      this.childImageSharp = new ChildImageSharp(attrs.childImageSharp)
+    this.childImageSharp = new ChildImageSharp(attrs.childImageSharp);
   }
 }
