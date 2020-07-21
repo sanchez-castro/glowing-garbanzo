fef69c41760eeb1caace1ac234e7444171513703
Jose Smiller <jose.smiller@axented.com>
2020-07-21 12:48:17 -0500

    Add lint corrections

--- src/shared/component/post-tags.tsx
+++ src/shared/component/post-tags.tsx
@@ -1,33 +1,31 @@
 import React from "react";
-import styles from './post-tags.module.scss'
-import {Link, navigate} from 'gatsby'
+import styles from "./post-tags.module.scss";
+import { Link, navigate } from "gatsby";
 import { kebabCase } from "lodash";
 
 interface Props {
-    tags: Array<string>
+  tags: Array<string>;
 }
 
 const Tags = (props: Props) => {
-
-    const selectTag = (tag: string) => {
-        navigate(
-            "/search-result/",
-            {
-                state: { searchTerm: "", tags: [tag] },
-            }
-        )
-    }
-    return(
-        <div className={styles.tags}>
-            {
-                props.tags.map((tag :string) => (
-                    <div key={tag} onClick={() => selectTag(tag)} className={styles.tag}>
-                        {tag}
-                    </div> 
-                ))
-            }
+  const selectTag = (tag: string) => {
+    navigate("/search-result/", {
+      state: { searchTerm: "", tags: [tag] },
+    });
+  };
+  return (
+    <div className={styles.tags}>
+      {props.tags.map((tag: string) => (
+        <div
+          key={tag}
+          onClick={() => selectTag(tag)}
+          className={[styles.tag, "small-text"].join(" ")}
+        >
+          {tag}
         </div>
-    ) 
-}
+      ))}
+    </div>
+  );
+};
 
-export default Tags
+export default Tags;
