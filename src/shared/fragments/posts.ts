1e084794a02b2ee4fd3b5fdfb99b12c66f94d130
Jose Smiller <jose.smiller@axented.com>
2020-07-21 12:50:05 -0500

    Add lint corrections

--- src/shared/fragments/posts.ts
+++ src/shared/fragments/posts.ts
@@ -1,37 +1,37 @@
 import { graphql } from "gatsby";
 
 export const postsFragment = graphql`
-    fragment PostsFragment on Query {
-      allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-        edges {
-          node {
-            id
-            excerpt(pruneLength: 400)
-            fields {
-              slug
-            }
-            frontmatter {
-              date(formatString: "MMMM DD, YYYY")
-              title
-              tags
-              contentType
-              topics
-              featuredImage {
-                childImageSharp {
-                  fluid(maxWidth: 800) {
-                    ...GatsbyImageSharpFluid
-                  }
+  fragment PostsFragment on Query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 400)
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            tags
+            contentType
+            topics
+            featuredImage {
+              childImageSharp {
+                fluid(maxWidth: 800) {
+                  ...GatsbyImageSharpFluid
                 }
               }
-              author {
-                id
-                bio
-                name
-                twitter
-              }
+            }
+            author {
+              id
+              bio
+              name
+              twitter
             }
           }
         }
       }
     }
+  }
 `;
