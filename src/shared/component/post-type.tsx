67d12df924d377e3c6510bef47ca8aeb55c296a2
Jose Smiller <jose.smiller@axented.com>
2020-07-21 12:48:45 -0500

    Add lint corrections

--- src/shared/component/post-type.tsx
+++ src/shared/component/post-type.tsx
@@ -1,37 +1,37 @@
 import React from "react";
-import styles from './post-type.module.scss'
-import articleIcon from '../../assets/icon/document.svg'
-import videoIcon from '../../assets/icon/video.svg'
+import styles from "./post-type.module.scss";
+import articleIcon from "../../assets/icon/document.svg";
+import videoIcon from "../../assets/icon/video.svg";
 
 interface Props {
-    types: Array<string>
+  types: Array<string>;
 }
 
 const PostType = (props: Props) => {
-    return (
-        <div className={styles.postTypes}>
-            {
-                props.types.map( type => (
-                    <div className={styles.postType} key={type}>
-                        <img src={checkType(type)} alt="post type" />
-                        <label className={styles.title}>{type}</label>
-                    </div>
-                ))
-            }
+  return (
+    <div className={styles.postTypes}>
+      {props.types.map((type) => (
+        <div className={styles.postType} key={type}>
+          <img src={checkType(type)} alt="post type" />
+          <label className={[styles.title, "small-text"].join(" ")}>
+            {type}
+          </label>
         </div>
-    )
-}
-
-const checkType = (type: string): string => {
-    type = type.toLowerCase();
-    switch (type) {
-      case "Article":
-        return articleIcon;
-      case "Video":
-        return videoIcon;
-      default:
-        return articleIcon;
-    }
+      ))}
+    </div>
+  );
 };
 
-export default PostType
+const checkType = (type: string): string => {
+  type = type.toLowerCase();
+  switch (type) {
+    case "artículo":
+      return articleIcon;
+    case "video":
+      return videoIcon;
+    default:
+      return articleIcon;
+  }
+};
+
+export default PostType;
