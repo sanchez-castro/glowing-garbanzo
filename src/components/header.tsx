8d2afd8072ced44d0426c0b28f3239ddab58b7ca
Jose Smiller <jose.smiller@axented.com>
2020-07-21 12:31:13 -0500

    Add header responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,60 +2,69 @@ import React, { Fragment, useState } from "react";
 import { Link, navigate } from "gatsby";
 import styles from "./header.module.scss";
 import { Container, Row, Col } from "react-bootstrap";
-import searchIcon from '../assets/icon/search.svg'
+import menuIcon from "../assets/icon/hamburguer.svg";
 
 interface HeaderProps {
   extendedSearchbar: boolean;
+  mobile?: boolean;
+  toggleHide: any;
 }
 
 const Header = (props: HeaderProps) => {
   const searchInputHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      const target = event.target as HTMLInputElement
-      navigate(
-        "/search-result/",
-        {
-          state: { searchTerm: target.value, tags: [] },
-        }
-      )
+    if (event.key === "Enter") {
+      const target = event.target as HTMLInputElement;
+      navigate("/search-result/", {
+        state: { searchTerm: target.value, tags: [] },
+      });
     }
-  }
+  };
   return (
     <header>
       <Container fluid>
-        <Row className={styles.headerRow}>
-          <Col lg={5} xl={5}>
-            <div className={styles.inputContainer}>
-              <input 
-                className={[
-                  props.extendedSearchbar ? "d-none" : "",
-                  styles.navSearch
-                ].join(" ")}
-                type="text"
-                placeholder="Search"
-                onKeyUp={searchInputHandler}
-              />
-              <img className={styles.inputIcon} src={searchIcon} alt=""/>
+        <Row className={[styles.headerRow, "justify-content-center"].join(" ")}>
+          {!props.mobile ? (
+            <Col lg={5} xl={5}>
+              <div className={styles.inputContainer}>
+                <input
+                  className={[
+                    props.extendedSearchbar ? "d-none" : "",
+                    styles.navSearch,
+                  ].join(" ")}
+                  type="text"
+                  placeholder="Buscar"
+                  onKeyUp={searchInputHandler}
+                />
+              </div>
+            </Col>
+          ) : (
+            <div className={styles.menuIcon}>
+              <img onClick={props.toggleHide} src={menuIcon} alt="menu icon" />
             </div>
-          </Col>
-          <Col lg={7} xl={7}>
-            <ul className={styles.navList}>
+          )}
+          <Col sm={12} md={7} lg={7}>
+            <ul
+              className={[
+                styles.navList,
+                props.mobile ? styles.navMobile : "",
+              ].join(" ")}
+            >
               <li>
                 <Link
                   className={styles.navItem}
                   activeClassName={styles.activeNavItem}
                   to="/"
                 >
-                  Home
+                  Inicio
                 </Link>
               </li>
               <li>
                 <Link
                   className={styles.navItem}
                   activeClassName={styles.activeNavItem}
-                  to="/perspectives"
+                  to="/about-us"
                 >
-                  Perspectives
+                  Acerca
                 </Link>
               </li>
               <li>
@@ -64,12 +73,18 @@ const Header = (props: HeaderProps) => {
                   activeClassName={styles.activeNavItem}
                   to="/philosophy"
                 >
-                  DPD Philosophy
+                  Filosofía
                 </Link>
               </li>
-              <li>
-                <button className={styles.navButton}>Subscribe</button>
-              </li>
+              {!props.mobile ? (
+                <li>
+                  <button className={[styles.navButton, "paragraph"].join(" ")}>
+                    Subscribir
+                  </button>
+                </li>
+              ) : (
+                ""
+              )}
             </ul>
           </Col>
         </Row>
