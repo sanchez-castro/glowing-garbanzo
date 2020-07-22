ef73590e901658be8f331e03b634dc4442bb87c9
Jose Smiller <jose.smiller@axented.com>
2020-07-22 16:25:59 -0500

    Add post type icon

--- src/shared/component/post-type.tsx
+++ src/shared/component/post-type.tsx
@@ -2,27 +2,43 @@ import React from "react";
 import styles from "./post-type.module.scss";
 import articleIcon from "../../assets/icon/document.svg";
 import videoIcon from "../../assets/icon/video.svg";
+import scientistIcon from "../../assets/icon/data-scientist.svg";
+import analystIcon from "../../assets/icon/data-analyst.svg";
+import biIcon from "../../assets/icon/bi-analyst.svg";
 
 interface Props {
   types: Array<string>;
-}
+  postType?: string;
+};
 
 const PostType = (props: Props) => {
   return (
     <div className={styles.postTypes}>
-      {props.types.map((type) => (
-        <div className={styles.postType} key={type}>
-          <img src={checkType(type)} alt="post type" />
+      {
+        props.types.map((type) => (
+          <div className={styles.postType} key={type}>
+            <img src={checkContentType(type)} alt="post type" />
+            <label className={[styles.title, "small-text"].join(" ")}>
+              {type}
+            </label>
+          </div>
+        ))
+      }
+      {props.postType ? (
+        <div className={styles.postType}>
+          <img className={styles.typeLogo} src={checkPostType(props.postType)} alt="" />
           <label className={[styles.title, "small-text"].join(" ")}>
-            {type}
+            {props.postType}
           </label>
         </div>
-      ))}
+      ) : (
+        ""
+      )}
     </div>
   );
 };
 
-const checkType = (type: string): string => {
+const checkContentType = (type: string): string => {
   type = type.toLowerCase();
   switch (type) {
     case "artículo":
@@ -34,4 +50,19 @@ const checkType = (type: string): string => {
   }
 };
 
+const checkPostType = (type: string) => {
+  type = type.toLowerCase();
+  switch (type) {
+    case "modulo":
+      return scientistIcon;
+    case "perspectiva":
+      return analystIcon;
+    case "proyecto":
+      return biIcon;
+    default:
+      return analystIcon;
+  }
+};
+
 export default PostType;
+
